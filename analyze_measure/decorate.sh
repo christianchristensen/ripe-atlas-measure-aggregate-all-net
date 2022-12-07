128e0fd52c2782df1b2d98794a255d46902fb912
Chris Christensen <christianchristensen@gmail.com>
2022-12-06 22:44:08 -0600

    Add notes and todo

--- analyze_measure/decorate.sh
+++ analyze_measure/decorate.sh
@@ -1,7 +1,7 @@
 UUID=$1
 
 curl -o asn.txt https://ftp.ripe.net/ripe/asnames/asn.txt
-echo "asn,name" > asn.csv
+echo "asn,asname" > asn.csv
 sed -e 's/,//g' asn.txt | awk '{print $1",\""$0"\""}' >> asn.csv
 echo "CSV Join ASN to RIPE Probe list"
 csvjoin -c asn_v4,asn ../create_measure/$UUID/ripe_probes$UUID.csv asn.csv > ripe_probes_asname$UUID.csv
